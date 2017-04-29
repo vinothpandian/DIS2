20d51d207ac97b859f2ab8b65ee8c56b4dd96195
Vinoth Pandian <vinoth.pandian@rwth-aachen.de>
2017-04-29 14:59:44 +0200

    Working Code without documentation

--- Assignment1/HelloWorldGraphical.java
+++ Assignment1/HelloWorldGraphical.java
@@ -1,138 +1,84 @@
-import java.awt.EventQueue;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import javax.swing.JSplitPane;
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Dimension;
 
-public class HelloWorldGraphical {
+public class HelloWorldGraphical implements ActionListener {
 
-    private JFrame frame;
+    JFrame frame = new JFrame();
+    //  Language selection menu
+    JButton button_Deutsch = new JButton("Deutsch");
+    JButton button_English = new JButton("English");
+    JButton button_French = new JButton("Francais");
 
-    /**
-     * Launch the application.
-     */
+    //  Message label
+    JLabel label_Message = new JLabel("Message");
+
+    //  Footer with Beenden button
+    JButton button_Beenden = new JButton("Beenden");
+
+    public HelloWorldGraphical(){
+        createMenu();
+    }
+
+    private void createMenu() {
+
+
+        //  Body Panel (top center)
+        JPanel panel_Body = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 15));
+        frame.getContentPane().add(panel_Body, BorderLayout.NORTH);
+
+        //  Language selection panel (inside body panel - center)
+        JPanel pLanguageSelection = new JPanel(new GridLayout(0, 1, 0, 10));
+        panel_Body.add(pLanguageSelection);
+
+        //  Buttons inside Language selection panel
+		button_Deutsch.addActionListener(this);
+        pLanguageSelection.add(button_Deutsch);
+
+        button_English.addActionListener(this);
+        pLanguageSelection.add(button_English);
+
+        button_French.addActionListener(this);
+        pLanguageSelection.add(button_French);
+
+        JPanel panel_Message = new JPanel(new FlowLayout());
+        frame.getContentPane().add(panel_Message, BorderLayout.CENTER);
+
+        panel_Message.add(label_Message);
+
+        JPanel panel_Bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
+        frame.getContentPane().add(panel_Bottom, BorderLayout.SOUTH);
+
+        button_Beenden.addActionListener(this);
+        panel_Bottom.add(button_Beenden);
+
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == button_Deutsch)
+            label_Message.setText("Guten Tag!");
+        else if(e.getSource() == button_English)
+            label_Message.setText("Good Day!");
+        else if(e.getSource() == button_French)
+            label_Message.setText("Bonne Journee!");
+        else if (e.getSource() == button_Beenden)
+            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    }
+
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    HelloWorldGraphical window = new HelloWorldGraphical();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+        // TODO Auto-generated method stub
+        HelloWorldGraphical hello = new HelloWorldGraphical();
 
-    /**
-     * Create the application.
-     */
-    public HelloWorldGraphical() {
-        initialize();
-    }
-
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-        //  Panels and buttons declaration
-        JPanel pBody = new JPanel();
-        JPanel pBodySpliter = new JPanel();
-
-        //  Language selection menu
-        JPanel pLangSelections = new JPanel();
-        JButton btnDeutsch = new JButton("Deutsch");
-        JButton btnEnglish = new JButton("English");
-        JButton btnFrancais = new JButton("Francais");
-
-        //  Message label
-        JPanel pMessage = new JPanel();
-        JLabel lblMessage = new JLabel("Message");
-
-        //  Footer with Beenden button
-        JPanel pFooter = new JPanel();
-        JButton btnBeenden = new JButton("Beenden");
-
-        // Window definition
-        frame = new JFrame("Hello GUI");
+        JFrame frame = new JFrame("Hello GUI");
         frame.setBounds(100, 100, 450, 300);
-        frame.setMinimumSize(new Dimension(200, 300));
+        frame.setMinimumSize(new Dimension(250, 300));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
-        //Window Body
-        FlowLayout fl_pBody = (FlowLayout) pBody.getLayout();
-        fl_pBody.setVgap(15);
-        frame.getContentPane().add(pBody, BorderLayout.CENTER);
-
-        pBody.add(pBodySpliter);
-        pBodySpliter.setLayout(new GridLayout(2, 1, 0, 0));
-
-        pBodySpliter.add(pLangSelections);
-        pLangSelections.setLayout(new GridLayout(3, 3, 0, 10));
-        //1st row
-
-
-        btnDeutsch.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                lblMessage.setText("Guten Tag!");
-            }
-        });
-        pLangSelections.add(btnDeutsch);
-
-
-        //2nd row
-
-
-        btnEnglish.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                lblMessage.setText("Hallo!");
-            }
-        });
-        pLangSelections.add(btnEnglish);
-
-        //3rd row
-
-        btnFrancais.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                lblMessage.setText("Bonjour! (or whatever it is)");
-            }
-        });
-        pLangSelections.add(btnFrancais);
-
-        pBodySpliter.add(pMessage);
-        FlowLayout flowLayout = (FlowLayout) pMessage.getLayout();
-        flowLayout.setVgap(25);
-        pMessage.add(lblMessage);
-
-
-        //  Beenden button at bottom right
-        FlowLayout fl_pFooter = (FlowLayout) pFooter.getLayout();
-        fl_pFooter.setVgap(15);
-        fl_pFooter.setHgap(15);
-        fl_pFooter.setAlignment(FlowLayout.RIGHT);
-        frame.getContentPane().add(pFooter, BorderLayout.SOUTH);
-
-        JButton btnBeenden = new JButton("Beenden");
-        btnBeenden.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-            }
-        });
-        pFooter.add(btnBeenden);
     }
 
 }
+
