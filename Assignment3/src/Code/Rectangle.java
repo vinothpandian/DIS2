051945d7ed86ca743d4824133e7cf04a67c2d41c
Vinoth Pandian <vinothpandian@rwth-aachen.de>
2017-05-14 17:51:07 +0200

    Moved to IDEA project

--- Assignment3/src/Code/Rectangle.java
+++ Assignment3/src/Code/Rectangle.java
@@ -12,5 +12,6 @@ public class Rectangle extends Object {
     public Rectangle(double startX, double startY, double endX, double endY) {
         super(startX, startY, endX, endY);
         this.color = color;
+
     }
 }
